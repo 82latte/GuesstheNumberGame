fcd39081a4bc23aee2dd14edb115de90522b9dda
82latte <hanako-uomura@awesomese.co.jp>
2025-11-14 06:30:45 +0000

    GuesstheNumberGameをアップロード
    GuesstheNumberGameをアップロードします。

--- GuesstheNumberGame.php
+++ GuesstheNumberGame.php
@@ -1,19 +1,27 @@
 <?php
-echo "Let's play the Guess the Number Game!".PHP_EOL;
-echo "Select fitst number : ";
+echo "数字を当てるゲームをしましょう!".PHP_EOL;
+echo "1つ目の数字を入力してください : ";
 $num1 = fgets(STDIN);
 
 //数字が入力されなければエラーを返す
 while(!is_numeric($num1)){
-    echo "Error. Select fitst 'number' : ";
+    echo "半角数字を入力してください : ";
     $num1 = fgets(STDIN);
 }
 
-echo "Select second number : ";
+echo "2つ目の数字を入力してください : ";
 $num2 = fgets(STDIN);
 
 while(!is_numeric($num2)){
-    echo "Error. Select second 'number' : ";
+    echo "半角数字を入力してください : ";
+    $num2 = fgets(STDIN);
+}
+
+//１つ目２つ目の数字が同じときは２つ目の数字を再入力
+while ($num2 == $num1){
+    $trimmedNum1 = trim($num1);
+
+    echo "1つ目の数字(".$trimmedNum1.")とは異なる数字を入力してください : " ;
     $num2 = fgets(STDIN);
 }
 
@@ -26,19 +34,19 @@ if ($num1 <= $num2) {
 
 $answer = 0;
 
-//５回に制限
+//回答できる回数を５回に制限
 for ($i = 5; $i > 0; $i--){
    
     if ($answer != $correct){
-        echo "Guess the number : ";
+        echo "数字を当ててください : ";
         $answer = fgets(STDIN);
     } else {
-    echo "Correct!".PHP_EOL;
+    echo "正解!".PHP_EOL;
     break;
     }
 }
 
 //５回で不正解のときの処理
 if ($answer != $correct){
-    echo "Gameover...".PHP_EOL;
+    echo "ゲームオーバー".PHP_EOL;
 }
